f97f8e7cee5db5f1b4b601f8e23077b0fb19bd3e
John Doe <johndoe@email.com>
2025-03-27 11:20:42 +0100

    verdere  testen

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-for i in range (10):
-    print (i)
-    
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("C:\\Users\\wai25\\OneDrive\\Desktop\\Periode 3\\tings\\Virtual folder\\Virtual-Human\\user_avatar_choices.csv", delimiter=',')
+
+print(df)
